21e2859733c1b14b3c78cfe579403ea00581a9ad
PorterZhang <porterzhang@aliyun.com>
2022-01-13 12:30:23 +0800

    [Add] 第一次直播课的作业

---
+++ 第一次课/作业/001-圆的面积.cpp
@@ -0,0 +1,15 @@
+/*
+  编写代码，在主函数内定义整型变量r为5，r表示半径，输出一个圆的面积。 
+*/
+# include <iostream>
+using namespace std; 
+
+int main() {
+	
+	int r = 5;
+	double pi = 3.14;
+	
+	cout << "半径r为：" << r << ",圆的面积为：" << pi * r * r << endl;
+	
+	return 0;
+}
---
+++ 第一次课/作业/002-两个数之和.cpp
@@ -0,0 +1,20 @@
+# include "iostream"
+using namespace std;
+
+int main() {
+	
+	int a;
+	int b;
+	int sum;
+	
+	cout << "请输入第一个数字:"; 
+	cin >> a;
+	cout << "请输入第二个数字:";
+	cin >> b;
+	
+	sum = a + b;
+	
+	cout << a << "+" << b << "=" << sum << endl;
+	
+	return  0;
+}
