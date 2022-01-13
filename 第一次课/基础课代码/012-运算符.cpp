743cd358d7c958c7c8f688c0252d82bda16b2109
PorterZhang <porterzhang@aliyun.com>
2022-01-13 10:49:54 +0800

    [Modified] 012-运算符.cpp

--- 第一次课/基础课代码/012-运算符.cpp
+++ 第一次课/基础课代码/012-运算符.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 	
 	printf("5/2=%d\n", 5/2); // 2
-	printf("5%2=%d\n", 5%2); // 1
+	printf("5%%2=%d\n", 5%2); // 1 这里的%%是表示的% 前一个%是转义 
 	
 	printf("5.0/2=%lf", 5.0/2); //2.5
 	
